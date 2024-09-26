f3a7513c93bfe5386e70ea0361fe9b4de13215cd
Johnsonyu0926 <97774037+Johnsonyu0926@users.noreply.github.com>
2024-09-26 12:11:43 +0800

    Update auditconfiger.h

--- source/h/auditconfiger.h
+++ source/h/auditconfiger.h
@@ -1,65 +1,28 @@
-#ifndef __CAUDIT_CONFIGER_H__
-#define __CAUDIT_CONFIGER_H__
+#ifndef AUDITCONFIGER_H
+#define AUDITCONFIGER_H
 
-class CAuditConfiger
-{
+#include <memory>
+#include "CDsList.h"
+
+class CAuditConfiger {
 public:
-	CDsList* GetAttaList(){
-		return m_pAttaList;
-	}
+    std::shared_ptr<CDsList> GetAttaList() const { return m_pAttaList; }
+    std::shared_ptr<CDsList> GetContList() const { return m_pContList; }
+    int GetAttaCheckMode() const { return m_nAttaCheckMode; }
+    int GetContCheckMode() const { return m_nContCheckMode; }
 
-	CDsList* GetContList(){
-		return m_pContList;
-	}
-	int GetAttaCheckMode() {
-		return m_nAttaCheckMode;
-	}
+    CAuditConfiger()
+        : m_pAttaList(std::make_shared<CDsList>()), m_pContList(std::make_shared<CDsList>()) {}
+    
+    ~CAuditConfiger() = default;
 
-	int GetContCheckMode(){
-		return m_nContCheckMode;
-	}
-
-public:
-	CAuditConfiger(){
-		m_pAttaList = new CDsList();
-		m_pContList = new CDsList();
-	}
-	~CAuditConfiger(){
-		if(m_pAttaList)
-		{
-			for(int i=0;i<m_pAttaList->GetCount();i++)	
-			{
-				char* pAtta = (char*)m_pAttaList->ElementAt(i);
-				delete []pAtta;
-	
-			}	
-			m_pAttaList->RemoveAll();
-			delete m_pAttaList;	
-			m_pAttaList = NULL;
-		}
-		if(m_pContList)	
-		{
-			for(int i=0;i<m_pContList->GetCount();i++)	
-			{
-				char* pCont = (char*)m_pContList->ElementAt(i);
-				delete []pCont;
-	
-			}	
-			m_pContList->RemoveAll();
-			delete m_pContList;	
-			m_pContList = NULL;
-			
-		}
-	}
+    bool Init(const std::string& szConf);
 
 private:
-	CDsList* m_pAttaList;
-	CDsList* m_pContList;
-	int m_nAttaCheckMode;
-	int m_nContCheckMode;
-	
-
-public:
-	BOOL Init(char* szConf);
+    std::shared_ptr<CDsList> m_pAttaList;
+    std::shared_ptr<CDsList> m_pContList;
+    int m_nAttaCheckMode;
+    int m_nContCheckMode;
 };
-#endif
+
+#endif // AUDITCONFIGER_H
