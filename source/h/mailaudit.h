d1b4b3a67b66969a21dd425758d5ef58c80ecabf
Johnsonyu0926 <97774037+Johnsonyu0926@users.noreply.github.com>
2024-09-27 14:17:42 +0800

    Update mailaudit.h

--- source/h/mailaudit.h
+++ source/h/mailaudit.h
@@ -1,89 +1,27 @@
-#ifndef __CMAILAUDIT_H__
-#define __CMAILAUDIT_H__
+#ifndef MAILAUDIT_H
+#define MAILAUDIT_H
 
-#include <gmime/gmime.h>
+#include <string>
+#include <vector>
+#include <memory>
+#include "Share.h"
 
-#define CHECK_WHITE	0
-#define CHECK_BLACK	1
+class CMailAudit {
+public:
+    CMailAudit();
+    virtual ~CMailAudit();
 
-#define AUDIT_PASS		0
-#define AUDIT_FORBIDEN_ATTACHMENT	1
-#define AUDIT_FORBIDEN_BODY		2
-#define AUDIT_FORBIDEN_SUBJECT		3
-class CQSearch;
-class CMailAudit
-{
-	public:
-		CMailAudit(){
-			
-			g_mime_init(0);
-			memset(m_szFoundKey,0,sizeof(m_szFoundKey));
-		}
-		~CMailAudit(){
-	
-			//g_mime_shutdown();
-		}
+    bool Init(const std::string& configFilePath);
+    bool Audit(const std::string& mailContent, std::vector<std::string>& issues);
 
-	public:
+private:
+    bool LoadConfig(const std::string& configFilePath);
+    bool CheckMailContent(const std::string& mailContent, std::vector<std::string>& issues);
 
-		char* GetSubject() {
-			return m_szSubject;
-		}
-		char* GetFrom() {
-			return m_szFrom;
-		}
-		char* GetTo(){
-			return m_szTo;
-		}
-		char* GetCc(){
-			return m_szCc;
-		}
-		char* GetDate(){
-			return m_szDate;
-		}
-
-
-		void SetAttaList(CDsList* pAttaList) {
-			m_pAttaList = pAttaList;
-		}
-
-		void SetContList(CDsList* pContList) {
-			m_pContList = pContList;
-		}
-
-		void SetContCheckMode(int nCheckMode) {
-			m_nContCheckMode = nCheckMode;
-		}
-
-		void SetAttaCheckMode(int nCheckMode) {
-			m_nAttaCheckMode = nCheckMode;
-		}
-
-		void SetQSearch(CQSearch* pQSearch) { m_pQSearch = pQSearch; }
-
-	public:
-		int Audit(char* pMail, int nMailSize);
-		BOOL GetFoundKey(char* szFoundKey,int nSize) 
-		{
-			strncpy(szFoundKey,m_szFoundKey,nSize);
-			return TRUE;
-		}
-	private:
-		char m_szFoundKey[128];
-		CDsList* m_pAttaList;
-		CDsList* m_pContList;
-		int m_nContCheckMode;
-		int m_nAttaCheckMode;
-		char* DoEncode(char* p);
-		BOOL DoDecode(char* p,char* pValue,int size);
-		BOOL DecodeSubject(char* szEnc,char* pValue,int size);
-		CQSearch* m_pQSearch;
-	
-	private:
-		char m_szSubject[1024];
-		char m_szFrom[1024];		
-		char m_szTo[1024];		
-		char m_szDate[1024];
-		char m_szCc[1024];
+    std::shared_ptr<CDsList> m_pAttaList;
+    std::shared_ptr<CDsList> m_pContList;
+    int m_nAttaCheckMode;
+    int m_nContCheckMode;
 };
-#endif
+
+#endif // MAILAUDIT_H
