9a7ffc61658a3822147289f974bd6bd1b935e177
Johnsonyu0926 <97774037+Johnsonyu0926@users.noreply.github.com>
2024-09-27 14:15:47 +0800

    Update icap.h

--- source/h/icap.h
+++ source/h/icap.h
@@ -1,74 +1,73 @@
-#ifndef __ICAP_H__
-#define __ICAP_H__
+#ifndef ICAP_H
+#define ICAP_H
+
+#include <string>
+#include <memory>
 #include "doorsbase.h"
-typedef struct ContentList{
-	char* Content;
-	int ContentLen;
-	int id;
-	struct ContentList *Next;
-}CList;
 
-class CIcap
-{
-public:
-	CIcap(){ 
-		MyList =new CList;
-		pList = MyList; 
-		MyList->Content = NULL;
-		MyList->ContentLen = 0;
-		MyList->Next = NULL;
-		m_pContent = NULL;
-	        m_nContentLen = 0;
-		count = 0;
-		}
-	~CIcap() { 
-		if(m_pContent) {
-			delete m_pContent;
-			m_pContent =NULL;
-		}
-	}
-		
-	void AddContent(char* szContent,int nLen)
-	{
-		DS_TRACE("ADD CONTENT \n"<<count);
-		count++;
-		CList *NewNode = new CList;
-		NewNode->Content = szContent;
-		NewNode->ContentLen = nLen;
-		NewNode->id = count;
-		NewNode->Next = NULL;
-		pList->Next =NewNode;
-		pList = pList->Next;
-		m_nContentLen = m_nContentLen + nLen;
-	}
-
-	void SetIcapServer(char* szServer,int nServerPort)
-	{
-		strncpy(m_szIcapServer,szServer,sizeof(m_szIcapServer));
-		m_nIcapServerPort = nServerPort;
-	}
-		
-
-	// use respmod to send to icap server and check the response.  
-	BOOL RespMod(char * pszAddress, int argflag,char* szVirus,int nLen);
-	void PrintContent()
-	{	
-		for (int i =0 ; i<count ; i++)
-		{
-			DS_TRACE("I IS : "<<i);
-		}
-	}
-
-	BOOL ReleaseList();
-	
-private:
-	char* m_pContent;
-	int m_nContentLen;
-	char m_szIcapServer[32];
-	int m_nIcapServerPort;
-	CList *MyList;
-	CList *pList;
-	BOOL IsFirst;
-	int count;
+struct ContentList {
+    std::string Content;
+    int ContentLen;
+    int id;
+    std::shared_ptr<ContentList> Next;
 };
-#endif
+
+class CIcap {
+public:
+    CIcap()
+        : MyList(std::make_shared<ContentList>()), pList(MyList),
+          m_nContentLen(0), count(0) {
+        MyList->Content = "";
+        MyList->ContentLen = 0;
+        MyList->Next = nullptr;
+        m_pContent = nullptr;
+    }
+
+    ~CIcap() {
+        if (m_pContent) {
+            delete[] m_pContent;
+            m_pContent = nullptr;
+        }
+    }
+
+    void AddContent(const std::string& szContent) {
+        DS_TRACE("ADD CONTENT \n" << count);
+        count++;
+        auto NewNode = std::make_shared<ContentList>();
+        NewNode->Content = szContent;
+        NewNode->ContentLen = szContent.length();
+        NewNode->id = count;
+        NewNode->Next = nullptr;
+        pList->Next = NewNode;
+        pList = pList->Next;
+        m_nContentLen += szContent.length();
+    }
+
+    void SetIcapServer(const std::string& szServer, int nServerPort) {
+        strncpy(m_szIcapServer, szServer.c_str(), sizeof(m_szIcapServer));
+        m_nIcapServerPort = nServerPort;
+    }
+
+    // 使用 respmod 发送到 icap 服务器并检查响应。
+    BOOL RespMod(char *pszAddress, int argflag, char *szVirus, int nLen);
+
+    void PrintContent() {
+        for (int i = 0; i < count; i++) {
+            DS_TRACE("I IS : " << i);
+        }
+    }
+
+    BOOL ReleaseList();
+
+private:
+    char *m_pContent;
+    int m_nContentLen;
+    char m_szIcapServer[32];
+    int m_nIcapServerPort;
+    std::shared_ptr<ContentList> MyList;
+    std::shared_ptr<ContentList> pList;
+    bool IsFirst;
+    int count;
+};
+
+#endif // ICAP_H
