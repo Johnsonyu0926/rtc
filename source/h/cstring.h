27bd9ba29e3a5d56f0f5cb26844d8d114ce92a2e
Johnsonyu0926 <97774037+Johnsonyu0926@users.noreply.github.com>
2024-09-29 14:07:03 +0800

    Update cstring.h

--- source/h/cstring.h
+++ source/h/cstring.h
@@ -1,10 +1,10 @@
-#ifndef _CSTRING_H__
-#define _CSTRING_H__
+#ifndef CSTRING_H
+#define CSTRING_H
 
+#include <string>
+#include <memory>
 #include "Share.h"
 #include "doorsbase.h"
-#include <string>
-#include <algorithm>
 
 constexpr int MAX_PRINTABLE_STRING_LENGTH = 5000;
 
@@ -13,28 +13,24 @@ public:
     CString();
     ~CString();
 
-    CString(const CString &rhs);
-    CString(const std::string &szBuf);
+    CString(const CString& rhs);
+    CString(const char* szBuf);
 
-    operator std::string() const {
-        return m_szBuf;
-    }
+    operator const char*() const { return m_szBuf.get(); }
 
-    char operator [](int nIndex) const {
+    char operator[](int nIndex) const {
         if (!m_szBuf || nIndex < 0 || nIndex >= m_nTotal || m_nTotal <= 0)
             return GAP_INITIALIZER;
         return m_szBuf[nIndex];
     }
 
-    CString &operator =(const CString &rhs);
-    CString &operator =(const std::string &szBuf);
-
-    const CString operator +(const CString &rhs) const;
-    const CString operator +(const std::string &szBuf) const;
-
-    CString &operator +=(const CString &rhs);
-    CString &operator +=(const std::string &szBuf);
-    CString &operator +=(char szCh);
+    CString& operator=(const CString& rhs);
+    CString& operator=(const char* szBuf);
+    CString operator+(const CString& rhs) const;
+    CString operator+(const char* szBuf) const;
+    CString& operator+=(const CString& rhs);
+    CString& operator+=(const char* szBuf);
+    CString& operator+=(char szCh);
 
     long GetLength() const;
     bool IsEmpty() const;
@@ -44,8 +40,8 @@ public:
     void SetAt(int nIndex, char ch);
     void Replace(char szSourceCh, char szDimCh);
 
-    int Compare(const std::string &szBuf) const;
-    int CompareNoCase(const std::string &szBuf) const;
+    int Compare(const char* szBuf) const; // 0代表两者相等，其他代表两者不等
+    int CompareNoCase(const char* szBuf) const; // 0代表两者相等，其他代表两者不等
 
     CString Mid(int nFirst) const;
     CString Mid(int nFirst, int nCount) const;
@@ -58,20 +54,20 @@ public:
     void TrimLeft();
     void TrimRight();
 
-    int Find(const std::string &szValue, int nStart = 0) const;
+    int Find(const char* szValue, int nStart = 0) const; // >= 0代表找到，< 0代表没找到
     int Find(char szCh, int sStat = 0) const;
 
-    void Format(const char *pFormat, ...);
+    void Format(const char* pFormat, ...);
 
 private:
-    bool GetValue(std::string &szValue) const;
-    bool AddValue(const std::string &szValue, long nLen);
+    bool GetValue(char* szValue) const;
+    bool AddValue(const char* szValue, long nLen);
     bool AddValue(char szCh, long nLen);
 
 private:
-    char *m_szBuf;
+    std::unique_ptr<char[]> m_szBuf;
     bool m_bFirstAdd;
     long m_nTotal;
 };
 
-#endif // _CSTRING_H__
+#endif // CSTRING_H
