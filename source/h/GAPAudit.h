0b7876d63e2a9e186c2335921e526e1216ce6e34
Johnsonyu0926 <97774037+Johnsonyu0926@users.noreply.github.com>
2024-09-29 10:53:18 +0800

    Update GAPAudit.h

--- source/h/GAPAudit.h
+++ source/h/GAPAudit.h
@@ -1,21 +1,17 @@
-#ifndef GAPAUDIT_H
-#define GAPAUDIT_H
+#ifndef _GAPAUDIT_H__
+#define _GAPAUDIT_H__
 
-#include <string>
-#include <vector>
-#include <array>
-#include <cstring>
-#include <memory>
+#define RULECOUNT 100
+
 #include "GAPMime.h"
-
-constexpr int RULECOUNT = 100;
+#include <string>
 
 class CGAPAudit {
 public:
     CGAPAudit();
     ~CGAPAudit();
 
-    bool AddContent(const uint8_t *pBuf, int nLen);
+    bool AddContent(uint8_t *pBuf, int nLen);
     bool InitRule();
     bool Audit(DsAuditStatus *pAudited);
 
@@ -25,24 +21,26 @@ public:
     std::string GetCc() const { return m_szCc; }
     std::string GetDate() const { return m_szDate; }
 
-    bool SetContent(const char* szBuf, int nLen);
+    bool SetContent(const std::string &szBuf, int nLen);
 
 private:
-    bool Decode(DsEncodingMethod EncodingMethod, const CString& strBody, CString& strDecoded);
-    bool AuditTextPlain(const uint8_t* pBody);
-    bool AuditHtml(const uint8_t* pBody);
-    bool AuditAttachment(const std::string& szFileName, int nFileType);
+    bool Decode(DsEncodingMethod EncodingMethod, const CString &strBody, CString &strDecoded);
+    bool AuditTextPlain(const uint8_t *pBody);
+    bool AuditHtml(const uint8_t *pBody);
+    bool AuditAttachment(const std::string &szFileName, int nFileType);
     int CheckFileName(const CString &strFileName);
     int AuditSubject();
 
 private:
     std::string m_szDate;
-    bool m_bReleaseMem;
+    int m_bReleaseMem;
+
     int m_nDenyAll;
     std::string m_szSubject;
     std::string m_szFrom;
     std::string m_szTo;
     std::string m_szCc;
+
     std::unique_ptr<char[]> m_szContent;
     long m_nContentLen;
     bool m_bFirstAdd;
@@ -53,7 +51,8 @@ private:
     int m_nRuleAttachMentCount;
     std::array<std::unique_ptr<char[]>, RULECOUNT> m_pRuleScript;
     int m_nRuleScriptCount;
+
     CGAPMime m_Mime;
 };
 
-#endif // GAPAUDIT_H
+#endif // _GAPAUDIT_H__
