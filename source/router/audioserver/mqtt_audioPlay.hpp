4b4e1a2c7056fdb0aceeddf76d790fedd338c3de
Johnsonyu0926 <97774037+Johnsonyu0926@users.noreply.github.com>
2024-09-30 14:05:50 +0800

    Update mqtt_audioPlay.hpp

--- source/router/audioserver/mqtt_audioPlay.hpp
+++ source/router/audioserver/mqtt_audioPlay.hpp
@@ -1,127 +1,83 @@
 #pragma once
 
-#include "json.hpp"
-#include "audiocfg.hpp"
-#include "add_mqtt_custom_audio_file.hpp"
-#include "utils.h"
-#include "volume.hpp"
+#include <string>
+#include <vector>
+#include <nlohmann/json.hpp>
+#include "mqtt.hpp"
+#include "audioPlay.hpp"
 
 using json = nlohmann::json;
 
-/*
- *  // mqtt发送体（即详细的功能参数，详细参数已JSON格式体展现）
-            {
-                "fileName":"65.mp3", // 播放音频文件�?
-                "audioUploadRecordId":389, // 播放音频文件上传id
-                "playCount":1, // 播放次数，单位次；当timeType为次数播放时生效
-                "playDuration":60, // 播放时长，单位秒，当timeType为时长播放时生效
-                "playStatus":1, // 播放状态，�?代表音频播放，详细参�?播放状态枚�?
-                "playType":1, // 播放模式�?-音频文件�?-文字播报�?-录音文件播放
-                "storageType":1, // 存储位置�?-内置Flash�?-外置存储, 可不�?
-                "timeType":0 // 0-时长播放�?-次数播放
-            }
+class MqttAudioPlay {
+public:
+    MqttAudioPlay(const std::string &clientId, const std::string &host, int port)
+        : clientId(clientId), host(host), port(port) {}
 
+    bool loadConfig(const std::string &configPath) {
+        try {
+            std::ifstream configFile(configPath);
+            if (!configFile.is_open()) return false;
 
-        {
-            "result":"播放成功", // 处理结果
-            "resultId":1, // 处理结果标记�?-表示未处理；1-表示处理成功�?-表示处理失败�?
-            "imei":"869298057401606", // 设备唯一序列�?
-            "topic":"TaDiao/device/report/test/869298057401606", // topic
-            "cmd":"audioPlay", // 当次处理的功能标�?
-            "publishId":172684, // 平台下发的本次请求id
-            "data": "" // 处理完成上传数据，如果有则需要上传，详情参照详细功能通信指令，已JSON格式体展�?
+            json j;
+            configFile >> j;
+
+            audioConfig = j.at("audioConfig").get<std::vector<AudioPlay>>();
+            return true;
+        } catch (const std::exception &e) {
+            std::cerr << "Error loading config file: " << e.what() << std::endl;
+            return false;
         }
- */
+    }
 
+    bool saveConfig(const std::string &configPath) const {
+        try {
+            json j = {
+                {"audioConfig", audioConfig}
+            };
 
+            std::ofstream configFile(configPath);
+            if (!configFile.is_open()) return false;
 
-namespace asns {
-    template<typename Quest, typename Result>
-    class CReQuest;
-
-    template<typename T>
-    class CResult;
-
-    class CAudioPlayResultData {
-    public:
-        NLOHMANN_DEFINE_TYPE_INTRUSIVE_WITH_DEFAULT(CAudioPlayResultData, null)
-
-        template<typename Quest, typename Result, typename T>
-        int do_success(const CReQuest<Quest, Result> &c, CResult<T> &r) {
-            CUtils utils;
-            if (CUtils::get_process_status("madplay") || CUtils::get_process_status("ffplay")) {
-                r.resultId = 2;
-                r.result = "Already played";
-                return 2;
-            }
-            CAudioCfgBusiness cfg;
-            cfg.load();
-
-            CAddMqttCustomAudioFileBusiness business;
-            if (business.exist(c.data.fileName) &&
-                utils.find_dir_file_exists(cfg.getAudioFilePath(), c.data.fileName) == false) {
-                business.deleteData(c.data.fileName);
-                r.resultId = 2;
-                r.result = "audio file does not exist";
-                return 2;
-            } else if (business.exist(c.data.fileName) &&
-                       utils.find_dir_file_exists(cfg.getAudioFilePath(), c.data.fileName)) {
-                switch (c.data.timeType) {
-                    case 0: {
-                        if (c.data.playDuration < 1) {
-                            r.resultId = 2;
-                            r.result = "play time error";
-                            return 2;
-                        }
-                        AcquisitionNoise::getInstance().noise_auto_set_volume();
-						AcquisitionNoise::getInstance().noiseDeque.clear();
-                        PlayStatus::getInstance().setMqttPlayStatus(c.data.playStatus);
-                        AudioPlayUtil::audio_time_play(c.data.playDuration, cfg.getAudioFilePath() + c.data.fileName,
-                                              ASYNC_START);
-                        break;
-                    }
-                    case 1: {
-                        if (c.data.playCount < 1) {
-                            r.resultId = 2;
-                            r.result = "play count error";
-                            return 2;
-                        }
-                        AcquisitionNoise::getInstance().noise_auto_set_volume();
-						AcquisitionNoise::getInstance().noiseDeque.clear();
-                        PlayStatus::getInstance().setMqttPlayStatus(c.data.playStatus);
-                        AudioPlayUtil::audio_num_play(c.data.playCount, cfg.getAudioFilePath() + c.data.fileName, ASYNC_START);
-                        break;
-                    }
-                }
-            } else {
-                r.resultId = 2;
-                r.result = "audio file does not exist";
-                return 2;
-            }
-            r.resultId = 1;
-            r.result = "success";
-            return 1;
+            configFile << j.dump(4);
+            return true;
+        } catch (const std::exception &e) {
+            std::cerr << "Error saving config file: " << e.what() << std::endl;
+            return false;
         }
+    }
 
-    public:
-        std::nullptr_t null;
-    };
+    void initializeClient() {
+        mqttClient = std::make_unique<MqttClient>(clientId, host, port);
+    }
 
+    void setCallback(const std::function<void(const std::string &topic, const std::string &message)> &callback) {
+        mqttClient->setMessageCallback(callback);
+    }
 
-    class CAudioPlayData {
-    public:
-        NLOHMANN_DEFINE_TYPE_INTRUSIVE_WITH_DEFAULT(CAudioPlayData, fileName, audioUploadRecordId, playCount,
-                                                    playDuration,
-                                                    playStatus, playType, storageType, timeType)
+    bool connect() {
+        return mqttClient->connect();
+    }
 
-    public:
-        std::string fileName;
-        int audioUploadRecordId;
-        int playCount;
-        int playDuration;
-        int playStatus;
-        int playType;
-        int storageType;
-        int timeType;
-    };
-}
+    void disconnect() {
+        mqttClient->disconnect();
+    }
+
+    bool subscribe(const std::string &topic) {
+        return mqttClient->subscribe(topic);
+    }
+
+    bool publish(const std::string &topic, const std::string &message) {
+        return mqttClient->publish(topic, message);
+    }
+
+    void loopForever() {
+        mqttClient->loopForever();
+    }
+
+private:
+    std::string clientId;
+    std::string host;
+    int port;
+    std::vector<AudioPlay> audioConfig;
+    std::unique_ptr<MqttClient> mqttClient;
+};
