b610e0fa861672a20200850529cb8127ca8e375f
Johnsonyu0926 <97774037+Johnsonyu0926@users.noreply.github.com>
2024-09-30 23:57:36 +0800

    Update HPR_Config.h

--- source/router/audioserver/include/HPR/HPR_Config.h
+++ source/router/audioserver/include/HPR/HPR_Config.h
@@ -1,91 +1,72 @@
-/*
- * CopyRight:    HangZhou Hikvision System Technology Co., Ltd. All    Right Reserved.
- * FileName:    HPR_Config.h
- * Desc:        OS Layer Component.
- * Author:        schina
- * Date:        2009-03-02
- * Contact:        zhaoyiji@hikvision.com.cn
- * History:        Created By Zhaoyiji 2009-03-02
- * */
-
-/*! \file HPR_Config.h
- */
-
 #ifndef __OS_CONFIG_H__
 #define __OS_CONFIG_H__
 
-#define  HPR_OS_NOT_SUPPORT "OS Not Implement Yet."
+#define HPR_OS_NOT_SUPPORT "OS Not Implement Yet."
 
-//////////////////////////////////////////////////////////////////////////
-//支持c++编译器
-#if defined (__cplusplus)
+#if defined(__cplusplus)
 #define CXXCOMPILE 1
 #endif
-//////////////////////////////////////////////////////////////////////////
 
 #if (defined(_WIN32) || defined(_WIN32_WCE) || defined(_WIN64))
 
-    #pragma warning(disable: 4819) //unicode字符警告
+    #pragma warning(disable: 4819)
 
-    //MSVC Version (vs98/vs2003/vs2005/vs2008/vs2010)
-    #if (_MSC_VER >= 1600)        //vs2010
+    #if (_MSC_VER >= 1600)
         #pragma warning(disable: 4996)
         #define MSVC_2010
-    #elif (_MSC_VER >= 1500)    //vs2008
+    #elif (_MSC_VER >= 1500)
         #pragma warning(disable: 4996)
         #define MSVC_2008
-    #elif (_MSC_VER >= 1400)    //vs2005
+    #elif (_MSC_VER >= 1400)
         #define MSVC_2005
-    #elif (_MSC_VER >= 1310)    //vs2003
+    #elif (_MSC_VER >= 1310)
         #define _WIN32_WINNT 0x0500
-        #define MSVC_2003        
-    #else                        //vs98
+        #define MSVC_2003
+    #else
         #pragma warning(disable: 4786)
         #pragma warning(disable: 4710)
         #define MSVC_98
     #endif
 
     #if defined(_WIN32_WCE)
-        #define OS_WINCE  1
+        #define OS_WINCE 1
     #endif
     #if defined(_WIN64)
-        #define OS_WINDOWS64    1
+        #define OS_WINDOWS64 1
     #endif
 
     #pragma warning(disable:4100)
     #pragma warning(disable:4127)
 
     #ifndef CALLBACK
-    #define CALLBACK __stdcall
+        #define CALLBACK __stdcall
     #endif
 
     #include <winsock2.h>
     #include <ws2tcpip.h>
-    #include <windows.h>    
+    #include <windows.h>
     #include <stdio.h>
     #include <stdlib.h>
 
-#ifdef WINAPI_FAMILY_PARTITION
-    #if (defined(WINAPI_PARTITION_PHONE) && (WINAPI_FAMILY_PARTITION(WINAPI_PARTITION_PHONE)))
-        #define OS_WINPHONE    1
-
-        #if (_WIN32_WINNT == _WIN32_WINNT_WIN8)
-            #define OS_WINPHONE8    1
+    #ifdef WINAPI_FAMILY_PARTITION
+        #if (defined(WINAPI_PARTITION_PHONE) && (WINAPI_FAMILY_PARTITION(WINAPI_PARTITION_PHONE)))
+            #define OS_WINPHONE 1
+            #if (_WIN32_WINNT == _WIN32_WINNT_WIN8)
+                #define OS_WINPHONE8 1
+            #endif
         #endif
     #endif
-#endif
 
-#ifndef OS_WINPHONE
-    #include <Mswsock.h>
-    //2012-02-07 链接hpr静态库需要此宏定义，链接动态库则不需要
-    #define HPR_STATIC
-#else
-    #define HPR_EXPORTS 1
-#endif    
+    #ifndef OS_WINPHONE
+        #include <Mswsock.h>
+        #define HPR_STATIC
+    #else
+        #define HPR_EXPORTS 1
+    #endif
     
-    #define OS_WINDOWS    1
-    #define HPR_INFINITE INFINITE    
-
+    #define OS_WINDOWS 1
+    #define HPR_INFINITE INFINITE
+    
     #ifdef HPR_STATIC
         #define HPR_DECLARE
         #define HPR_DECLARE_CLASS
@@ -104,7 +85,7 @@
 
 #elif (defined(__linux__) || defined(__APPLE__) || defined(__SYMBIAN32__) || defined(ANDROID) || defined(__ANDROID__))
 
-    #include <unistd.h>    
+    #include <unistd.h>
     #include <sys/socket.h>
     #include <sys/ioctl.h>
     #include <netinet/in.h>
@@ -122,38 +103,38 @@
         #include <sys/stat.h>
     #endif
 
-    #define OS_POSIX    1
+    #define OS_POSIX 1
     #define HPR_INFINITE 0xFFFFFFFF
-    #define WAIT_TIMEOUT    258
+    #define WAIT_TIMEOUT 258
 
     #if defined(__LP64__)
-        #define OS_POSIX64    1
+        #define OS_POSIX64 1
     #endif
 
     #if defined (__APPLE__)
-        #include<TargetConditionals.h>
+        #include <TargetConditionals.h>
         #include <sys/stat.h>
         #include <sys/sockio.h>
 
-        #define OS_APPLE    1
+        #define OS_APPLE 1
 
         #if (defined(TARGET_OS_IPHONE) && (TARGET_OS_IPHONE == 1))
-            #define OS_IPHONE   1        
+            #define OS_IPHONE 1
         #elif (defined(TARGET_OS_MAC) && (TARGET_OS_MAC == 1))
-            #define OS_MAC   1
+            #define OS_MAC 1
         #endif
     #endif
 
     #if defined(__SYMBIAN32__)
-        #define OS_S60        1
+        #define OS_S60 1
     #endif
 
     #if defined(ANDROID) || defined(__ANDROID__)
-        #define OS_ANDROID        1
+        #define OS_ANDROID 1
     #endif
     
     #if ((defined(__arm__) || defined(__ARMEL__)) && defined(__linux__))
-        #define OS_ARMLINUX        1
+        #define OS_ARMLINUX 1
     #endif
 
     #ifdef HPR_STATIC
@@ -161,7 +142,7 @@
     #else
         #define HPR_DECLARE extern "C"
     #endif
-    #define HPR_DECLARE_CLASS 
+    #define HPR_DECLARE_CLASS
     #define HPR_DECLARE_CXX
 
     #ifndef CALLBACK
@@ -175,15 +156,15 @@
 
     #error OS Not Implement Yet.
 
-#endif    // end #if (defined(WIN32) || defined(_WIN32_WCE))
+#endif
 
-#define    HPR_OK            (0)
-#define HPR_ERROR        (-1)
-#define HPR_NOSUPPORT    (-2)
+#define HPR_OK (0)
+#define HPR_ERROR (-1)
+#define HPR_NOSUPPORT (-2)
 
 #define HPR_TIMEOUT (0)
 #define HPR_FOREVER (-1)
 
 #define OS_SUPPORT_IPV6 1
 
-#endif    // end #ifndef __OS_CONFIG_H__
+#endif // __OS_CONFIG_H__
