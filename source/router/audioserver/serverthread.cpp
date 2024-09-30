ca6ddc918be4bcad878626a77d530272a62759d9
Johnsonyu0926 <97774037+Johnsonyu0926@users.noreply.github.com>
2024-09-30 14:48:58 +0800

    Update serverthread.cpp

--- source/router/audioserver/serverthread.cpp
+++ source/router/audioserver/serverthread.cpp
@@ -9,43 +9,50 @@
 BOOL CServerThread::InitInstance() {
     CSocket socket;
     socket.Create(TCP);
+    
     int opt = 1;
-    setsockopt(socket.m_hSocket, SOL_SOCKET, SO_REUSEADDR, (const char *) &opt, sizeof(opt));
+    setsockopt(socket.m_hSocket, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
 
     if (!socket.Bind(m_nPort)) {
-        LOG(INFO) << "Failed bind port:" << m_nPort;
+        LOG(INFO) << "Failed to bind port: " << m_nPort;
         exit(1);
     }
+
     socket.Listen();
     int i = 0;
-    while (1) {
+
+    while (true) {
         struct timeval timeout;
         timeout.tv_sec = 5;
         timeout.tv_usec = 0;
-
         fd_set rset;
         FD_ZERO(&rset);
         FD_SET(socket.m_hSocket, &rset);
+
         CUtils utils;
         utils.heart_beat("/tmp/audio_server_netvoice_heartbeat.txt");
-        int n = select(socket.m_hSocket + 1, &rset, NULL, NULL, &timeout);
+
+        int n = select(socket.m_hSocket + 1, &rset, nullptr, nullptr, &timeout);
         if (n < 0) {
-            LOG(WARNING) << "fatal , select error!";
+            LOG(WARNING) << "Fatal error: select error!";
             return 0;
-        }else if(n == 0){
+        } else if (n == 0) {
             continue;
-        } else if (n > 0) {
-            LOG(INFO) << "server select n = " << n;
+        } else {
+            LOG(INFO) << "Server select n = " << n;
         }
-        CSocket *pClient = new CSocket;
+
+        auto *pClient = new CSocket;
         socket.Accept(pClient);
-        LOG(INFO) << "Got the no."<<i<<" connection :"<<pClient->GetRemoteIp()<<":"<<ntohs(pClient->GetPeerPort());
+        LOG(INFO) << "Got the no." << i << " connection: " << pClient->GetRemoteIp() << ":" << ntohs(pClient->GetPeerPort());
         Singleton::getInstance().m_connIp = pClient->GetRemoteIp();
         i++;
-        CClientThread *pThread = new CClientThread();
+
+        auto *pThread = new CClientThread;
         pThread->SetClient(pClient);
         pThread->CreateThread();
     }
+
     return TRUE;
 }
 
