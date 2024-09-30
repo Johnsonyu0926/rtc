db20e3f8c7fca0db2307af3a2c753c84c0a90e3f
Johnsonyu0926 <97774037+Johnsonyu0926@users.noreply.github.com>
2024-09-30 14:43:41 +0800

    Update public.hpp

--- source/router/audioserver/public.hpp
+++ source/router/audioserver/public.hpp
@@ -1,99 +1,108 @@
 #pragma once
+
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <map>
+#include <set>
+#include <deque>
+#include <list>
+#include <algorithm>
+#include <iterator>
+#include <functional>
+#include <memory>
+#include <thread>
+#include <mutex>
+#include <condition_variable>
+#include <future>
+#include <atomic>
+#include <chrono>
+#include <random>
+#include <iomanip>
+#include <cstdlib>
+#include <cstdio>
+#include <cstdint>
+#include <cstring>
+#include <cmath>
+#include <ctime>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/time.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <dirent.h>
+#include <cerrno>
+#include <csignal>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <netinet/ip.h>
+#include <netinet/tcp.h>
+#include <netdb.h>
+#include <ifaddrs.h>
+#include <sys/ioctl.h>
+#include <sys/socket.h>
+#include <sys/wait.h>
+#include <linux/sockios.h>
+#include <linux/if_packet.h>
+#include <linux/if_ether.h>
+#include <linux/if_arp.h>
+#include <linux/if_tun.h>
+#include <linux/if.h>
+#include <errno.h>
+#include <stdarg.h>
+#include <stdio.h>
+#include "rapidjson/document.h"
+#include "rapidjson/writer.h"
+#include "rapidjson/stringbuffer.h"
+
 namespace asns {
-    const int BUFSIZE = 8192;
 
-    const int AUDIO_FILE_TYPE = 32;
+class Utils {
+public:
+    static std::string exec(const char* cmd) {
+        std::array<char, 128> buffer;
+        std::string result;
+        std::unique_ptr<FILE, decltype(&pclose)> pipe(popen(cmd, "r"), pclose);
+        if (!pipe) {
+            throw std::runtime_error("popen() failed!");
+        }
+        while (fgets(buffer.data(), buffer.size(), pipe.get()) != nullptr) {
+            result += buffer.data();
+        }
+        return result;
+    }
 
-    const int BEGINPORT = 34509;
-    const int ENDPORT = 34608;
+    static std::string hex_to_string(const std::string& input) {
+        static const char* const lut = "0123456789ABCDEF";
+        size_t len = input.length();
+        if (len & 1) throw std::invalid_argument("odd length");
 
-    const int TCPNETWORKSET      = 101;
-    const int UPDATEPWD          = 102;
-    const int NETWORKSET         = 104;
-    const int LOGIN              = 103;
-    const int SETDEVICEADDRRESS  = 0;
-    const int AUDIOPLAY = 1;
-    const int AUDIOSTOP = 2;
-    const int VOLUMSET = 3;
-    const int REBOOT = 4;
-    const int GETDEVICESTATUS = 5;
-    const int TTSPLAY = 6;
-    const int LIGHTSWITCH = 7;
-    const int FILEUPLOAD = 8;
-    const int RESTORE = 9;
-    const int AUDIONUMORTPLAY = 10;
-    const int TTSNUMORTPLAY = 11;
-    const int GETDEVICEBASEINFO = 12;
-    const int RECORD = 13;
-    const int REMOTEUPGRADE = 14;
-    const int GETAUDIOLIST = 15;
-    const int LIGHTCONFIG = 16;
-    const int RECORDBEGIN = 18;
-    const int RECORDEND = 19;
-    const int AUDIOFILEUPLOAD = 20;
-    const int REMOTEFILEUPGRADE = 21;
-    const int DSPMANAGE = 22;
+        std::string output;
+        output.reserve(len / 2);
+        for (size_t i = 0; i < len; i += 2) {
+            char a = input[i];
+            const char* p = std::lower_bound(lut, lut + 16, a);
+            if (*p != a) throw std::invalid_argument("not a hex digit");
 
-    const char *const BEGIN_END_CODE_ERROR  = "F1";
-    const char *const LENGTH_ERROR          = "F2";
-    const char *const CHECK_CODE_ERROR      = "F3";
-    const char *const NONSUPPORT_ERROR      = "F4";
-    const char *const OPERATION_FAIL_ERROR  = "F5";
-    const char *const USER_PWD_ERROR        = "F6";
-    //const char *const TCP_TIMEOUT = "F7";
-    const char *const OPERATION_NEW_IP_ERROR = "F11";
-    const char *const OPERATION_NEW_AUDIO_FILE_NAME_ERROR = "F21";
-    const char *const ALREADY_PLAYED = "F22";
-    const char *const AUDIO_FILE_NOT_EXITS = "F23";
-    const char *const NETWORK_SET_ERROR = "F24";
-    const char *const OPERATION_NEW_PWD_COMPLEXITY_ERROR = "F25";
-    const char *const NEW_PWD_SET_ERROR = "F26";
-    const char *const AUDIO_SUFFIX_ERROR = "F27";
-    const char *const DOWNLOAD_FILE_ERROR = "F28";
-    const char *const FLASH_LIGHT_SET_ERROR = "F30";
-    const char *const VOLUME_RANGE_ERROR = "F31";
-    const char *const TTS_TXT_LENGTH_ERROR = "F41";
-    const char *const RECORD_TIME_ERROR = "F60";
-    const char *const POST_ERROR = "F61";
-    const char *const GET_ERROR = "F65";
-    const char *const RECORD_SIZE_ERROR = "F62";
-    const char *const TCP_TRANSFER_ERROR = "F66";
-    const char *const TCP_UPGRADE_ERROR = "F70";
-    const char *const TCP_TIMEOUT = "F71";
-    const char *const LACK_SPACE = "F72";
-    const char *const AUDIO_SIZE_LARGE = "F73";
+            char b = input[i + 1];
+            const char* q = std::lower_bound(lut, lut + 16, b);
+            if (*q != b) throw std::invalid_argument("not a hex digit");
 
-    const char *const RECORD_PATH = "/tmp/record.mp3";
-    const char *const FIRMWARE_PATH = "/var/run/SONICCOREV100R001.bin";
+            output.push_back(((p - lut) << 4) | (q - lut));
+        }
+        return output;
+    }
 
-    const std::string TTS_PATH = "/tmp/output.wav";
+    static void network_set(const std::string& gateway, const std::string& ipAddress, const std::string& netMask) {
+        std::ostringstream oss;
+        oss << "ifconfig eth0 " << ipAddress << " netmask " << netMask;
+        exec(oss.str().c_str());
+        oss.str("");
+        oss << "route add default gw " << gateway;
+        exec(oss.str().c_str());
+    }
+};
 
-    const int STOP_TASK_PLAYING = -1;
-    const int TIMED_TASK_PLAYING = 1;
-    const int TALK_TASK_PLAYING = 2;
-    const int AUDIO_TASK_PLAYING = 3;
-    const int TTS_TASK_PLAYING = 4;
-
-    const bool ASYNC_START = true;
-
-    const int SINGLE_PLAY = 1;
-
-    const int GPIO_CUSTOM_MODE = 1;
-    const int GPIO_PLAY_MODE = 2;
-
-    const int GPIO_CLOSE = 0;
-    const int GPIO_OPEN = 1;
-
-    const int MQTT_HEART_BEAT_TIME = 10;
-
-    const int FEMALE = 0;
-    const int MALE = 1;
-
-    const int RECORD_TIME_COMPENSATE = 2;
-    const std::string RECORD_CMD = "ffmpeg -t 200 -y -f alsa -sample_rate 44100 -i hw:0,0 -acodec libmp3lame -ar 8k /tmp/record.mp3";
-
-    static std::string REQUEST_TOPIC;
-    const int DISK_SIZE = 5120;
-
-	const std::string led_check = "55AA0000";
-}
+} // namespace asns
