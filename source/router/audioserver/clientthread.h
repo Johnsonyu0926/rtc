41e5fa0402c9c8fdbac932c5851eb70250884d56
Johnsonyu0926 <97774037+Johnsonyu0926@users.noreply.github.com>
2024-09-30 17:54:03 +0800

    Update clientthread.h

--- source/router/audioserver/clientthread.h
+++ source/router/audioserver/clientthread.h
@@ -1,32 +1,35 @@
 #ifndef __CLIENT_THREAD_H__
 #define __CLIENT_THREAD_H__
 
+#include "json.hpp"
+#include <vector>
+#include <string>
+
 class CSThread;
 class CSocket;
-#include "json.hpp"
+
 using json = nlohmann::json;
-class CClientThread : public CSThread
-{
-public:
-	virtual BOOL InitInstance();
-	virtual BOOL ExitInstance();
 
+class CClientThread : public CSThread {
 public:
-	void SetClient(CSocket *pClient) { m_pClient = pClient; }
+    CClientThread() : m_pClient(nullptr) {}
+    virtual ~CClientThread() = default;
+
+    virtual BOOL InitInstance();
+    virtual BOOL ExitInstance();
+    void SetClient(CSocket *pClient) { m_pClient = pClient; }
 
 private:
-	CSocket *m_pClient;
-	BOOL Check(const unsigned char *szBuf);
-	BOOL Gen(BYTE *szBuf); // make sure the buf size is 1024 Bytes.
-	int do_req(char *buf, CSocket *pClient);
-	int do_verify(char *buf);
-	int do_str_req(CSocket *pClient);
-	int do_str_verify(char *buf, CSocket *pClient);
+    BOOL Check(const unsigned char *szBuf);
+    BOOL Gen(BYTE *szBuf); // ensure the buffer size is 1024 Bytes
+    int do_req(char *buf, CSocket *pClient);
+    int do_verify(char *buf);
+    int do_str_req(CSocket *pClient);
+    int do_str_verify(char *buf, CSocket *pClient);
 
-private:
-	json m_json;
-	std::vector<std::string> m_str;
-	//int m_login_status;
+    CSocket *m_pClient;
+    json m_json;
+    std::vector<std::string> m_str;
 };
 
-#endif
+#endif // __CLIENT_THREAD_H__
