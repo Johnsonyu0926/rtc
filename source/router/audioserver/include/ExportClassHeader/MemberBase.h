2b066e243286d1a59d31c148cd7dc9a8af8a79a5
Johnsonyu0926 <97774037+Johnsonyu0926@users.noreply.github.com>
2024-09-30 15:22:03 +0800

    Update MemberBase.h

--- source/router/audioserver/include/ExportClassHeader/MemberBase.h
+++ source/router/audioserver/include/ExportClassHeader/MemberBase.h
@@ -1,53 +1,37 @@
-/**  @file   MemberBase.h
- *   @note   HangZhou Hikvision System Technology Co., Ltd. All Right Reserved. 
- *   @brief 
- *   
- *   @author shijianfengyf2
- *   @modify shijianfengyf2
- *   @date   2018/09/12
- *   
- *   @note:
- */
 #ifndef _EBASE_BASE_HANDLEMGR_MEMBERBASE_H_
 #define _EBASE_BASE_HANDLEMGR_MEMBERBASE_H_
 
 #include "DevComPublicDef.h"
 
-namespace DevEBase
-{
-    class NET_COM_CLASS CMemberBasePrivate
-    {
-    public:
-        friend class CMemberBase;
-        CMemberBasePrivate()
-        {
-            m_iMemberIndex = -1;
-        }
+namespace DevEBase {
 
-        ~CMemberBasePrivate(){}
-    private:
-        HPR_INT32 m_iMemberIndex;
-    };
+class CMemberBasePrivate {
+public:
+    friend class CMemberBase;
+    CMemberBasePrivate() : m_iMemberIndex(-1) {}
 
-    /**    @class CMemberBase
-    *    @brief 
-    */
-    class NET_COM_CLASS CMemberBase
-    {
-    public:
-        CMemberBase();
-        virtual ~CMemberBase();
-        virtual HPR_BOOL CheckResource();
+    ~CMemberBasePrivate() = default;
 
-        virtual HPR_BOOL Start(void *pParams) = 0;
-        virtual HPR_BOOL Stop() = 0;
+private:
+    HPR_INT32 m_iMemberIndex;
+};
 
-        HPR_BOOL SetMemberIndex(HPR_INT32 iMemberIndex);
-        HPR_INT32 GetMemberIndex();
+class NET_COM_CLASS CMemberBase {
+public:
+    CMemberBase();
+    virtual ~CMemberBase();
+    virtual HPR_BOOL CheckResource();
 
-    private:
-        CMemberBasePrivate* m_pPrivate;
-    };
-}
+    virtual HPR_BOOL Start(void *pParams) = 0;
+    virtual HPR_BOOL Stop() = 0;
+
+    HPR_BOOL SetMemberIndex(HPR_INT32 iMemberIndex);
+    HPR_INT32 GetMemberIndex() const;
+
+private:
+    CMemberBasePrivate* m_pPrivate;
+};
+
+} // namespace DevEBase
 
 #endif //_EBASE_BASE_HANDLEMGR_MEMBERBASE_H_
