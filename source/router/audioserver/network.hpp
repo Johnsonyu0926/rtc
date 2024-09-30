91a41f187709c5688dbd1192617fc47cdc34e382
Johnsonyu0926 <97774037+Johnsonyu0926@users.noreply.github.com>
2024-09-30 14:41:28 +0800

    Update network.hpp

--- source/router/audioserver/network.hpp
+++ source/router/audioserver/network.hpp
@@ -1,51 +1,50 @@
-#ifndef __CNETWORKSET_H__
-#define __CNETWORKSET_H__
+#pragma once
 
-#include <iostream>
-#include "json.hpp"
+#include <string>
+#include <nlohmann/json.hpp>
 #include "utils.h"
+#include "CSocket.h"
 
-using namespace std;
+using json = nlohmann::json;
 
 namespace asns {
 
-    class CNetworkSetResult {
-    private:
-        string cmd;
-        int resultId;
-        string msg;
+class CNetworkSetResult {
+public:
+    NLOHMANN_DEFINE_TYPE_INTRUSIVE(CNetworkSetResult, cmd, resultId, msg)
 
-    public:
-        NLOHMANN_DEFINE_TYPE_INTRUSIVE(CNetworkSetResult, cmd, resultId, msg)
+    void do_success() {
+        cmd = "NetworkSet";
+        resultId = 1;
+        msg = "NetworkSet handle success";
+    }
 
-        void do_success() {
-            cmd = "NetworkSet";
-            resultId = 1;
-            msg = "NetworkSet handle success";
-        };
-    };
+private:
+    std::string cmd;
+    int resultId;
+    std::string msg;
+};
 
-    class CNetworkSet {
-    private:
-        string cmd;
-        string ipAddress;
-        string gateway;
-        string netMask;
+class CNetworkSet {
+public:
+    NLOHMANN_DEFINE_TYPE_INTRUSIVE(CNetworkSet, cmd, ipAddress, gateway, netMask)
 
-    public:
-        NLOHMANN_DEFINE_TYPE_INTRUSIVE(CNetworkSet, cmd, ipAddress, gateway, netMask)
+    int do_req(CSocket *pClient) {
+        CUtils utils;
+        CNetworkSetResult res;
+        res.do_success();
+        json j = res;
+        std::string s = j.dump();
+        pClient->Send(s.c_str(), s.length());
+        utils.network_set(gateway, ipAddress, netMask);
+        return 1;
+    }
 
-        int do_req(CSocket *pClient) {
-            CUtils utils;
-            CNetworkSetResult res;
-            res.do_success();
-            json j = res;
-            std::string s = j.dump();
-            pClient->Send(s.c_str(), s.length());
-            utils.network_set(gateway, ipAddress, netMask);
-            return 1;
-        }
-    };
+private:
+    std::string cmd;
+    std::string ipAddress;
+    std::string gateway;
+    std::string netMask;
+};
 
-} // namespace tcpserver
-#endif
+}  // namespace asns
