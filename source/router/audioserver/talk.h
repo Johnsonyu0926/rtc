6d851dd44e4b75b162fb0323e19152a45c2a60de
Johnsonyu0926 <97774037+Johnsonyu0926@users.noreply.github.com>
2024-09-30 14:51:08 +0800

    Update talk.h

--- source/router/audioserver/talk.h
+++ source/router/audioserver/talk.h
@@ -6,46 +6,37 @@
 #include "HPR_Types.h"
 
 #define NON_PLAY_PRIORITY 100
-class CBlock
-{
+
+class CBlock {
 public:
-    CBlock() {}
-    ~CBlock()
-    {
-        if (buf)
-        {
+    CBlock() : buf(nullptr), len(0) {}
+    ~CBlock() {
+        if (buf) {
             free(buf);
         }
     }
     char *buf;
     int len;
 };
-class CTalk
-{
-public:
-    CTalk()
-    {
-        m_bInit = 0;
 
-        fp = NULL;
-    }
-    ~CTalk() {}
+class CTalk {
+public:
+    CTalk() : m_bInit(0), fp(nullptr) {}
+    ~CTalk() = default;
 
 private:
-    FILE *fp = NULL;
+    FILE *fp;
     int m_bInit;
     int initBase();
 
 protected:
-    static int RecvMsg(ezDevSDK_talk_msg_to_dev *msg);           ///< 消息回调接口，不可以阻塞，主动接收消息和回调方式二选一，实现了回调接口，消息就走回调函数
-    static int GetRuntimeInfo(ezDevSDK_talk_runtime_info *info); ///< SDK运行时获取信息回调，不能有任何阻塞操作
+    static int RecvMsg(ezDevSDK_talk_msg_to_dev *msg);
+    static int GetRuntimeInfo(ezDevSDK_talk_runtime_info *info);
     static int RecvTalkData(int channel, char *data, int len);
     static int CheckResource(int cur_count, int channel);
-    static int RecvTalkDataNew(int channel, char *data, int len, int encode_type); // NPQ对讲走这个回调
-
+    static int RecvTalkDataNew(int channel, char *data, int len, int encode_type);
     static int BaseFunctionRecvMsg(ezDevSDK_base_function_msg_to_dev *msg);
     static int BaseFunctionGetRuntimeInfo(ezDevSDK_base_function_runtime_info *info);
-
     static int BaseFunctionOtaNotifier(int percent, void *data, int data_len, ezDevSDK_down_err_code errcode, ezDevSDK_down_status status);
 
 public:
