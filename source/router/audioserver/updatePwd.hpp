233d6ce0aa042a438c28765bad92457640a07f1c
Johnsonyu0926 <97774037+Johnsonyu0926@users.noreply.github.com>
2024-09-30 14:57:10 +0800

    Update updatePwd.hpp

--- source/router/audioserver/updatePwd.hpp
+++ source/router/audioserver/updatePwd.hpp
@@ -1,64 +1,65 @@
 #pragma once
 
+#include <string>
 #include "json.hpp"
 #include "audiocfg.hpp"
 #include "utils.h"
 
 namespace asns {
-    class CUpdateResult {
-    public:
-        NLOHMANN_DEFINE_TYPE_INTRUSIVE(CUpdateResult, cmd, resultId, msg)
 
-        void do_souccess() {
-            cmd = "UpdatePwd";
-            resultId = 1;
-            msg = "update password success";
+class CUpdateResult {
+public:
+    NLOHMANN_DEFINE_TYPE_INTRUSIVE(CUpdateResult, cmd, resultId, msg)
+
+    void do_success() {
+        cmd = "UpdatePwd";
+        resultId = 1;
+        msg = "update password success";
+    }
+
+    void do_fail() {
+        cmd = "UpdatePwd";
+        resultId = 2;
+        msg = "update password fail";
+    }
+
+private:
+    std::string cmd;
+    int resultId;
+    std::string msg;
+};
+
+class CUpdate {
+public:
+    NLOHMANN_DEFINE_TYPE_INTRUSIVE(CUpdate, cmd, password, oldPassword)
+
+    int do_req(CSocket *pClient) {
+        CAudioCfgBusiness cfg;
+        cfg.load();
+
+        CUpdateResult updateResult;
+
+        if (oldPassword != cfg.business[0].serverPassword) {
+            updateResult.do_fail();
+        } else {
+            CUtils utils;
+            utils.change_password(password.c_str());
+            cfg.business[0].serverPassword = password;
+            cfg.saveToJson();
+            updateResult.do_success();
         }
 
-        void do_fail() {
-            cmd = "UpdatePwd";
-            resultId = 2;
-            msg = "update password fail";
-        }
+        json resp = updateResult;
+        std::string str = resp.dump();
+        pClient->Send(str.c_str(), str.length());
 
-    private:
-        std::string cmd;
-        int resultId;
-        std::string msg;
-    };
+        return (updateResult.resultId == 1) ? 1 : 0;
+    }
 
-    class CUpdate {
-    public:
-        NLOHMANN_DEFINE_TYPE_INTRUSIVE(CUpdate, cmd, password, oldPassword)
+private:
+    std::string cmd;
+    std::string password;
+    std::string oldPassword;
+};
 
-        int do_req(CSocket *pClient) {
-            CAudioCfgBusiness cfg;
-            cfg.load();
-            if (oldPassword != cfg.business[0].serverPassword) {
-                CUpdateResult updateResult;
-                updateResult.do_fail();
-                json resp = updateResult;
-                std::string str = resp.dump();
-                pClient->Send(str.c_str(), str.length());
-                return 0;
-            } else {
-                CUtils utils;
-				utils.change_password(password.c_str());
-
-                cfg.business[0].serverPassword = password;
-                cfg.saveToJson();
-                CUpdateResult updateResult;
-                updateResult.do_souccess();
-                json resp = updateResult;
-                std::string str = resp.dump();
-                pClient->Send(str.c_str(), str.length());
-                return 1;
-            }
-        }
-
-    private:
-        std::string cmd;
-        std::string password;
-        std::string oldPassword;
-    };
-}
+} // namespace asns
