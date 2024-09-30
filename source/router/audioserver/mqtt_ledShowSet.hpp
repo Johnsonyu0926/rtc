07d730864cb25a1f77f2471c55d2b5dea701323c
Johnsonyu0926 <97774037+Johnsonyu0926@users.noreply.github.com>
2024-09-30 14:22:39 +0800

    Update mqtt_ledShowSet.hpp

--- source/router/audioserver/mqtt_ledShowSet.hpp
+++ source/router/audioserver/mqtt_ledShowSet.hpp
@@ -1,102 +1,82 @@
 #pragma once
 
-#include "json.hpp"
-#include "mqtt_reTemp.hpp"
-#include "utils.h"
-#include "Rs485NoiseMange.hpp"
-#include "rs485Manage.hpp"
+#include <string>
+#include <nlohmann/json.hpp>
+#include "mqtt.hpp"
+#include "ledShowSet.hpp"
+
 using json = nlohmann::json;
 
-namespace asns {
-    template<typename Quest, typename Result>
-    class CReQuest;
+class MqttLedShowSet {
+public:
+    MqttLedShowSet(const std::string &clientId, const std::string &host, int port)
+        : clientId(clientId), host(host), port(port) {}
 
-    template<typename T>
-    class CResult;
+    bool loadConfig(const std::string &configPath) {
+        try {
+            std::ifstream configFile(configPath);
+            if (!configFile.is_open()) return false;
 
-    class CLedShowSetResultData {
-    public:
-        NLOHMANN_DEFINE_TYPE_INTRUSIVE_WITH_DEFAULT(CLedShowSetResultData, ledShowRecordId, data)
+            json j;
+            configFile >> j;
 
-        static void recover_state() {
-            AcquisitionNoise::getInstance().setMonitorStatus(AcquisitionNoise::getInstance().getMonitorStatus() ? 0 : 1);
-            if (AcquisitionNoise::getInstance().getMonitorStatus()) {
-                CUtils::async_wait(1, 0, 0, [&] {
-                    Rs485NoiseMange::startCollectingNoise();
-                });
-            } else {
-                CUtils::async_wait(1, 0, 0, [&] {
-                    RSBusinessManage rs;
-                    rs.worker();
-                });
-            }
+            ledShowConfig = j.at("ledShowConfig").get<LedShowSet>();
+            return true;
+        } catch (const std::exception &e) {
+            std::cerr << "Error loading config file: " << e.what() << std::endl;
+            return false;
         }
-        template<typename Quest, typename Result, typename T>
-        int do_success(const CReQuest<Quest, Result> &c, CResult<T> &r) {
-            if(c.data.ledCommand.size() == 0){
-                r.resultId = 2;
-                r.result = "failed ledCommand size == 0";
-                return 2;
-            }
-            CUtils utils;
-            std::string str;
-            for(const auto &i : c.data.ledCommand){
-                str += utils.hex_to_string(i.substr(2));
-            }
-            ledShowRecordId = c.data.ledShowRecordId;
-            AcquisitionNoise::getInstance().setMonitorStatus(AcquisitionNoise::getInstance().getMonitorStatus() ? 0 : 1);
-            while (Rs485::get_rs485_state()) {
-                std::this_thread::sleep_for(std::chrono::milliseconds(100));
-            }
-            const int fd = Rs485::_uart_open();
-            if (fd < 0) {
-                r.resultId = 2;
-                r.result = "failed to open ttyS";
-                recover_state();
-                return 2;
-            }
-            Rs485::_uart_write(str.c_str(), str.length());
-            tcflush(fd, TCIOFLUSH);
-            unsigned char buf[255] = {0};
-            int read_count = Rs485::while_select(fd, buf, sizeof(buf), 3);
-            Rs485::set_send_dir();
-            LOG(INFO) << "read_count:" << read_count;
-            if (read_count < 0) {
-                r.resultId = 2;
-                r.result = "no response";
-                Rs485::uart_close();
-                recover_state();
-                return 2;
-            }
-            data = CUtils::ustr_to_hex(buf, read_count);
-            LOG(INFO) << "buf: " << data;
-            if (data.compare(0, led_check.length(), led_check) != 0) {
-                r.resultId = 2;
-                r.result = "Execution failure";
-                Rs485::uart_close();
-                recover_state();
-                return 2;
-            }
-            Rs485::uart_close();
-            recover_state();
-            r.resultId = 1;
-            r.result = "success";
-            return 1;
+    }
+
+    bool saveConfig(const std::string &configPath) const {
+        try {
+            json j = {
+                {"ledShowConfig", ledShowConfig}
+            };
+
+            std::ofstream configFile(configPath);
+            if (!configFile.is_open()) return false;
+
+            configFile << j.dump(4);
+            return true;
+        } catch (const std::exception &e) {
+            std::cerr << "Error saving config file: " << e.what() << std::endl;
+            return false;
         }
+    }
 
-    public:
-        long ledShowRecordId;
-        std::string data;
-    };
+    void initializeClient() {
+        mqttClient = std::make_unique<MqttClient>(clientId, host, port);
+    }
 
+    void setCallback(const std::function<void(const std::string &topic, const std::string &message)> &callback) {
+        mqttClient->setMessageCallback(callback);
+    }
 
-    class CLedShowSetData {
-    public:
-        NLOHMANN_DEFINE_TYPE_INTRUSIVE_WITH_DEFAULT(CLedShowSetData, ledCommand, showContent, ledShowRecordId)
+    bool connect() {
+        return mqttClient->connect();
+    }
 
-    public:
-        std::vector<std::string> ledCommand;
-        std::string showContent;
-        long ledShowRecordId;
-    };
-}
+    void disconnect() {
+        mqttClient->disconnect();
+    }
+
+    bool subscribe(const std::string &topic) {
+        return mqttClient->subscribe(topic);
+    }
+
+    bool publish(const std::string &topic, const std::string &message) {
+        return mqttClient->publish(topic, message);
+    }
+
+    void loopForever() {
+        mqttClient->loopForever();
+    }
+
+private:
+    std::string clientId;
+    std::string host;
+    int port;
+    LedShowSet ledShowConfig;
+    std::unique_ptr<MqttClient> mqttClient;
+};
