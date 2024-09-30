71b8fdf81504db8e2c6cb74e7ae7f5d75036c7b2
Johnsonyu0926 <97774037+Johnsonyu0926@users.noreply.github.com>
2024-09-30 13:42:00 +0800

    Update dodownload.sh

--- source/router/audioserver/dodownload.sh
+++ source/router/audioserver/dodownload.sh
@@ -1,1 +1,10 @@
-curl -A "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.110 Safari/537.36" "$1" -L -o $2
+#!/bin/sh
+
+if [ "$#" -ne 2 ]; then
+    echo "Usage: $0 <URL> <Output File>"
+    exit 1
+fi
+
+URL="$1"
+OUTPUT_FILE="$2"
+curl -A "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.110 Safari/537.36" "$URL" -L -o "$OUTPUT_FILE"
