06b06146a0ae49e960b0bfbf1ed6f89cb75a7f35
Johnsonyu0926 <97774037+Johnsonyu0926@users.noreply.github.com>
2024-09-30 19:27:20 +0800

    Update Singleton.h

--- source/router/audioserver/include/PublicUtils/Singleton.h
+++ source/router/audioserver/include/PublicUtils/Singleton.h
@@ -1,36 +1,30 @@
-/**  @file   Singleton.h
- *   @note   HangZhou Hikvision System Technology Co., Ltd. All Right Reserved. 
- *   @brief  单例模板，全局类基类
- *   
- *   @author shijianfengyf2
- *   @modify shijianfengyf2
- *   @date   2018/08/09
- *   
- *   @note:
- */
+#pragma once
+#include <mutex>
+#include <memory>
 
-#include "HPR/HPR_Config.h"
-#include "HPR/HPR_Types.h"
+namespace PublicUtils {
 
-namespace DevComUtils
-{
-    template <typename T>
-    class CSingleton
-    {
-    public:
-        static T* GetInstance();
-        virtual ~CSingleton();
-    protected:
-        CSingleton(){}; //阻止别人进行构造函数
-    };
-
-    template <typename T>
-    T* CSingleton<T>::GetInstance()
-    {
-        static T instance;  //这里不考虑线程安全问题，不上锁
-                            //因为在EHome组件里，单例都是全局的globalctrl类
-                            //而这个类在init的时候就会调用，此时一般是只有一个线程
-        return &instance;
+template <typename T>
+class Singleton {
+public:
+    static T& Instance() {
+        static std::once_flag s_flag;
+        std::call_once(s_flag, [&]() { m_pInstance.reset(new T()); });
+        return *m_pInstance;
     }
+
+    Singleton(const Singleton&) = delete;
+    Singleton& operator=(const Singleton&) = delete;
+
+protected:
+    Singleton() = default;
+    virtual ~Singleton() = default;
+
+private:
+    static std::unique_ptr<T> m_pInstance;
+};
+
+template <typename T>
+std::unique_ptr<T> Singleton<T>::m_pInstance;
 
 }
