e9524dfa05c9f6656fd0892418268df7e45f03be
Johnsonyu0926 <97774037+Johnsonyu0926@users.noreply.github.com>
2024-09-30 14:44:54 +0800

    Update relaySet.hpp

--- source/router/audioserver/relaySet.hpp
+++ source/router/audioserver/relaySet.hpp
@@ -1,47 +1,58 @@
 #pragma once
 
+#include <string>
 #include "json.hpp"
 #include "utils.h"
 
 namespace asns {
-    class CRelaySetResult {
-    public:
-        NLOHMANN_DEFINE_TYPE_INTRUSIVE(CRelaySetResult, cmd, resultId, msg)
 
-        void do_success() {
-            cmd = "RelaySet";
-            resultId = 1;
-            msg = "RelaySet success";
-        }
+class CRelaySetResult {
+public:
+    NLOHMANN_DEFINE_TYPE_INTRUSIVE(CRelaySetResult, cmd, resultId, msg)
 
-        void do_fail(const std::string& str) {
-            cmd = "RelaySet";
-            resultId = 2;
-            msg = str;
-        }
+    void do_fail(const std::string& str) {
+        cmd = "RelaySet";
+        resultId = 2;
+        msg = str;
+    }
 
-    private:
-        std::string cmd;
-        int resultId;
-        std::string msg;
-    };
+    void do_success() {
+        cmd = "RelaySet";
+        resultId = 1;
+        msg = "RelaySet success";
+    }
 
-    class CRelaySet {
-    public:
-        NLOHMANN_DEFINE_TYPE_INTRUSIVE(CRelaySet, cmd, model, status)
-
-        int do_req(CSocket *pClient) {
-            Relay::getInstance().set_gpio_model(model, status);
-            CRelaySetResult relaySetResult;
-            relaySetResult.do_success();
-            json js = relaySetResult;
-            std::string s = js.dump();
-            return pClient->Send(s.c_str(), s.length());
-        }
-
-    private:
-        std::string cmd;
-        int model;
-        int status;
-    };
+private:
+    std::string cmd;
+    int resultId;
+    std::string msg;
 };
+
+class CRelaySet {
+public:
+    NLOHMANN_DEFINE_TYPE_INTRUSIVE(CRelaySet, cmd, relayState)
+
+    int do_req(CSocket *pClient) {
+        CUtils utils;
+        CRelaySetResult res;
+        res.do_success();
+        if (relayState.empty()) {
+            res.do_fail("relayState is empty");
+        } else {
+            std::string str = utils.hex_to_string(relayState);
+            if (Rs485::_uart_work(str.c_str(), str.length()) != 1) {
+                res.do_fail("relayState write fail");
+            }
+        }
+        json j = res;
+        std::string s = j.dump();
+        pClient->Send(s.c_str(), s.length());
+        return 1;
+    }
+
+private:
+    std::string cmd;
+    std::string relayState;
+};
+
+} // namespace asns
