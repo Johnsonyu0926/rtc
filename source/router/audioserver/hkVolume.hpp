a1b2bfb188d3c4ad2fb7f0801a6f090e5a25f39d
Johnsonyu0926 <97774037+Johnsonyu0926@users.noreply.github.com>
2024-09-30 13:50:58 +0800

    Update hkVolume.hpp

--- source/router/audioserver/hkVolume.hpp
+++ source/router/audioserver/hkVolume.hpp
@@ -1,85 +1,78 @@
 #pragma once
 
-#include <iostream>
-#include "volume.hpp"
-#include "json.hpp"
-#include "utils.h"
+#include <string>
+#include <fstream>
+#include <nlohmann/json.hpp>
 
 using json = nlohmann::json;
 
-namespace asns {
-    class CHKVolumeInfo {
-    public:
-        NLOHMANN_DEFINE_TYPE_INTRUSIVE(CHKVolumeInfo, audioOutID, volume)
+class HkVolume {
+public:
+    HkVolume() = default;
+    ~HkVolume() = default;
 
-    public:
-        int audioOutID;
-        int volume;
-    };
+    std::string getId() const { return id; }
+    void setId(const std::string &newId) { id = newId; }
 
-    class CHKVolume {
-    public:
-        explicit CHKVolume() = default;
+    int getVolume() const { return volume; }
+    void setVolume(int newVolume) { volume = newVolume; }
 
-        ~CHKVolume() = default;
+    json toJson() const {
+        return json{
+            {"id", id},
+            {"volume", volume}};
+    }
 
-        void parse(const std::string &res) {
-			try {
-					json js = json::parse(res);
-					if (strstr(res.c_str(),"data") && strstr(res.c_str(), "Value") && strstr(res.c_str(),"BroadcastAudioCfgList")) {
-							info = js.at("data").at("Value").at("BroadcastAudioCfgList");
-					} else {
-						LOG(WARNING) << "not found data key.";
-						return ;
-					}
-			} catch (json::parse_error &ex) {
-                LOG(ERROR) << "parse error at byte " << ex.byte;
-                return ;
-            }
-            volume = info[0].volume < 10 && info[0].volume > 0 ? 10 : info[0].volume;
-            volume /= 10;
-            volume *= 10;
-            this->volumeSet(volume);
+    static HkVolume fromJson(const json &j) {
+        return HkVolume(
+            j.at("id").get<std::string>(),
+            j.at("volume").get<int>());
+    }
+
+private:
+    std::string id;
+    int volume;
+};
+
+class HkVolumeManager {
+public:
+    HkVolumeManager(const std::string &configPath) : configPath(configPath) {}
+
+    bool load() {
+        try {
+            std::ifstream configFile(configPath);
+            if (!configFile.is_open()) return false;
+
+            json j;
+            configFile >> j;
+
+            volume = HkVolume::fromJson(j);
+            return true;
+        } catch (const std::exception &e) {
+            std::cerr << "Error loading config file: " << e.what() << std::endl;
+            return false;
         }
+    }
 
-        void volumeSet(const int volume) {
-            CUtils utils;
-            if (utils.is_ros_platform()) {
-                CVolumeSet v;
-                v.setVolume(vo_ros[volume]);
-                v.addj(vo_ros[volume]);
-                v.saveToJson();
-            }else{
-                char buf[64] = {0};
-                sprintf (buf, "amixer set Headphone Playback %d", vo[volume]);
-                system(buf);
-            }
+    bool save() const {
+        try {
+            json j = volume.toJson();
+
+            std::ofstream configFile(configPath);
+            if (!configFile.is_open()) return false;
+
+            configFile << j.dump(4);
+            return true;
+        } catch (const std::exception &e) {
+            std::cerr << "Error saving config file: " << e.what() << std::endl;
+            return false;
         }
+    }
 
-    private:
-        int volume;
-        std::vector<CHKVolumeInfo> info;
-        std::map<int, int> vo{{0,   0},
-                              {10,  100},
-                              {20,  100},
-                              {30,  100},
-                              {40,  103},
-                              {50,  105},
-                              {60,  110},
-                              {70,  115},
-                              {80,  120},
-                              {90,  125},
-                              {100, 130}};
-        std::map<int, int> vo_ros{{0,   0},
-                                  {10,  1},
-                                  {20,  1},
-                                  {30,  2},
-                                  {40,  2},
-                                  {50,  3},
-                                  {60,  4},
-                                  {70,  5},
-                                  {80,  6},
-                                  {90,  6},
-                                  {100, 7}};
-    };
-}
+    HkVolume getVolume() const { return volume; }
+    void setVolume(const HkVolume &newVolume) { volume = newVolume; }
+
+private:
+    std::string configPath;
+    HkVolume volume;
+};
