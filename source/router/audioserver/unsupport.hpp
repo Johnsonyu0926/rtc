9c5b04467eb6b21303e4e97c9419d1a77cb9eadf
Johnsonyu0926 <97774037+Johnsonyu0926@users.noreply.github.com>
2024-09-30 14:56:40 +0800

    Update unsupport.hpp

--- source/router/audioserver/unsupport.hpp
+++ source/router/audioserver/unsupport.hpp
@@ -1,50 +1,50 @@
-#ifndef __CUNSUPPORT_H__
-#define __CUNSUPPORT_H__
-#include <iostream>
+#pragma once
+
+#include <string>
 #include "json.hpp"
-using namespace std;
+#include "CSocket.h"
+#include "log.h"
 
-namespace asns
-{
+namespace asns {
 
-	class CUnsupport
-	{
-	private:
-		string cmd;
-		int resultId;
-		string msg;
+class CUnsupport {
+public:
+    NLOHMANN_DEFINE_TYPE_INTRUSIVE(CUnsupport, cmd, resultId, msg)
 
-	public:
-		NLOHMANN_DEFINE_TYPE_INTRUSIVE(CUnsupport, cmd, resultId, msg)
+    void do_success(const std::string& reqcmd) {
+        cmd = reqcmd;
+        resultId = 2;
+        msg = "unsupport operation";
+    }
 
-	public:
-		void do_success(const std::string& reqcmd) {
-			cmd = reqcmd;
-			resultId = 2;
-			msg = "unsupport operation";
-		}
-	};
-	class CUnsupportBusiness
-	{
-	private:
-		string cmd;
+private:
+    std::string cmd;
+    int resultId;
+    std::string msg;
+};
 
-	public:
-		NLOHMANN_DEFINE_TYPE_INTRUSIVE(CUnsupportBusiness, cmd)
+class CUnsupportBusiness {
+public:
+    NLOHMANN_DEFINE_TYPE_INTRUSIVE(CUnsupportBusiness, cmd)
 
-	public:
-		void setCmd(string unsupport_cmd) { cmd = unsupport_cmd; }
-		int do_req(CSocket *pClient)
-		{
-            LOG(INFO) << "unsuport cmd:" << cmd;
-			CUnsupport res;
-			res.do_success(cmd);
-			json j = res;
-			std::string s = j.dump();
-			pClient->Send(s.c_str(), s.length());
-			return 1;
-		}
-	};
+    void setCmd(const std::string& unsupport_cmd) {
+        cmd = unsupport_cmd;
+    }
 
-} // namespace tcpserver
-#endif
+    int do_req(CSocket *pClient) {
+        LOG(INFO) << "unsupport cmd: " << cmd;
+        CUnsupport res;
+        res.do_success(cmd);
+
+        json j = res;
+        std::string s = j.dump();
+        pClient->Send(s.c_str(), s.length());
+
+        return 1;
+    }
+
+private:
+    std::string cmd;
+};
+
+} // namespace asns
