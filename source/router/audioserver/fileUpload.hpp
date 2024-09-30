9539a46ed38ec9c64a0e184b0c857c31b93caf3a
Johnsonyu0926 <97774037+Johnsonyu0926@users.noreply.github.com>
2024-09-30 13:44:37 +0800

    Update fileUpload.hpp

--- source/router/audioserver/fileUpload.hpp
+++ source/router/audioserver/fileUpload.hpp
@@ -1,75 +1,56 @@
 #pragma once
 
-#include "json.hpp"
-#include "utils.h"
-#include "add_custom_audio_file.hpp"
+#include <string>
+#include <vector>
+#include <fstream>
+#include <iostream>
+#include <nlohmann/json.hpp>
 
-namespace asns {
-    class CFileUploadResult {
-    public:
-        NLOHMANN_DEFINE_TYPE_INTRUSIVE(CFileUploadResult, cmd, resultId, msg)
+using json = nlohmann::json;
 
-        void do_success() {
-            cmd = "FileUpload";
-            resultId = 1;
-            msg = "file upload success";
-        }
+class FileUpload {
+public:
+    FileUpload() = default;
+    ~FileUpload() = default;
 
-        void do_fail(const std::string& str) {
-            cmd = "FileUpload";
-            resultId = 2;
-            msg = str;
-        }
+    std::string getFileName() const { return fileName; }
+    void setFileName(const std::string &newFileName) { fileName = newFileName; }
 
-    private:
-        std::string cmd;
-        int resultId;
-        std::string msg;
-    };
-
-    class CFileUpload {
-    public:
-        NLOHMANN_DEFINE_TYPE_INTRUSIVE(CFileUpload, downloadUrl, fileName)
-
-        int do_req(CSocket *pClient) {
-            std::transform(fileName.begin(), fileName.end(), fileName.begin(), ::tolower);
-            CFileUploadResult fileUploadResult;
-            CUtils utils;
-            if(utils.get_available_Disk("/mnt") < asns::DISK_SIZE){
-                fileUploadResult.do_fail("Lack of space");
-                json js = fileUploadResult;
-                std::string str = js.dump();
-                LOG(INFO) << "return json: " << str;
-                return pClient->Send(str.c_str(), str.length());
+    bool uploadFile(const std::string &filePath, const std::string &destination) {
+        try {
+            std::ifstream source(filePath, std::ios::binary);
+            if (!source.is_open()) {
+                std::cerr << "Failed to open source file: " << filePath << std::endl;
+                return false;
             }
-            CAudioCfgBusiness cfg;
-            std::string res = utils.get_upload_result(downloadUrl.c_str(), cfg.getAudioFilePath().c_str(),
-                                                      fileName.c_str());
-            if (res.compare("success") != 0) {
-                fileUploadResult.do_fail(res);
-            } else {
-                CAddCustomAudioFileData node;
-                node.filePath = downloadUrl;
-                node.customAudioName = fileName;
-                CAddCustomAudioFileBusiness business;
-                utils.bit_rate_32_to_48(cfg.getAudioFilePath() + fileName);
-                if (!business.exist(fileName)) {
-                    business.business.push_back(node);
-                    business.saveToJson();
-                    fileUploadResult.do_success();
-                } else {
-                    fileUploadResult.do_success();
-                }
-            }
-            json js = fileUploadResult;
-            std::string str = js.dump();
-            LOG(INFO) << "return json: " << str;
-            return pClient->Send(str.c_str(), str.length());
-        }
 
-    private:
-        std::string cmd;
-        std::string downloadUrl;
-        std::string fileName;
-    };
-}
+            std::ofstream dest(destination, std::ios::binary);
+            if (!dest.is_open()) {
+                std::cerr << "Failed to open destination file: " << destination << std::endl;
+                return false;
+            }
+
+            dest << source.rdbuf();
+
+            source.close();
+            dest.close();
+
+            return true;
+        } catch (const std::exception &e) {
+            std::cerr << "Exception during file upload: " << e.what() << std::endl;
+            return false;
+        }
+    }
+
+    json toJson() const {
+        return json{
+            {"fileName", fileName}};
+    }
+
+    static FileUpload fromJson(const json &j) {
+        return FileUpload(j.at("fileName").get<std::string>());
+    }
+
+private:
+    std::string fileName;
+};
